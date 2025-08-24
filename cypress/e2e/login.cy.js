2a98c2201ff26e79ed4a493085b0414e3e2f3b11
faizman0 <auliahersa@gmail.com>
2025-08-24 09:45:55 +0700

    testcase1

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login OrangeHRM', () => {
+  beforeEach( () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  });
+  it('Login dengan username dan password benar', () => {
+    cy.get('input[name="username"]').type('Admin');
+    cy.get('input[name="password"]').type('admin123');
+    cy.get('button[type="submit"]').click();
+
+    //verifikasi berhasil login
+    cy.url().should('include', '/web/index.php/dashboard/index');
   })
 })
